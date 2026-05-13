3e9e0a78681c63d03326688fb69fdcf9cd15765d
Konouluc0 <konouluc0@gmail.com>
2026-05-13 20:58:36 +0000

    coorection

--- src/infrastructure/logger/logger.ts
+++ src/infrastructure/logger/logger.ts
@@ -1,16 +1,18 @@
 import pino from "pino";
 import { env } from "../../config/env";
 
+const usePrettyTransport =
+  env.NODE_ENV !== "production" && typeof process.env.VERCEL === "undefined";
+
 export const logger = pino({
   level: env.NODE_ENV === "production" ? "info" : "debug",
-  transport:
-    env.NODE_ENV === "production"
-      ? undefined
-      : {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            translateTime: "SYS:standard",
-          },
+  transport: usePrettyTransport
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
         },
+      }
+    : undefined,
 });
