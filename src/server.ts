d6cdfbf17c94bc2018a92fe759f1667809477411
Konouluc0 <konouluc0@gmail.com>
2026-05-13 20:18:32 +0000

    coorection servless

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,13 @@ import { app } from "./app";
 import { env } from "./config/env";
 import { logger } from "./infrastructure/logger/logger";
 
-app.listen(env.PORT, () => {
-  logger.info(`Server running on http://localhost:${env.PORT}`);
-  logger.info(`Swagger docs on http://localhost:${env.PORT}/api/docs`);
-});
+const isVercel = process.env.VERCEL === "1";
+
+if (!isVercel) {
+  app.listen(env.PORT, () => {
+    logger.info(`Server running on http://localhost:${env.PORT}`);
+    logger.info(`Swagger docs on http://localhost:${env.PORT}/api/docs`);
+  });
+}
+
+export default app;
